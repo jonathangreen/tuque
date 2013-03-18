910761c8d5ce781c8cd31dbd9dcd187698fa1dac
William Panting <willtp87@hotmail.com>
2013-03-18 14:17:37 -0300

    UUID in constructObject tests namespaces in constructObject tests

--- tests/RepositoryTest.php
+++ tests/RepositoryTest.php
@@ -24,10 +24,15 @@ class RepositoryTest extends PHPUnit_Framework_TestCase {
     $object->state = 'd';
     $object->owner = 'woot';
     $this->repository->ingestObject($object);
+
+    $id_array = explode(':', $object->id);
+    $this->assertEquals($namespace, $id_array[0]);
+
     $this->assertTrue($object instanceof FedoraObject);
     $this->assertEquals('foo', $object->label);
     $this->assertEquals('D', $object->state);
     $this->assertEquals('woot', $object->owner);
+
     $this->repository->purgeObject($object->id);
   }
 
@@ -38,11 +43,13 @@ class RepositoryTest extends PHPUnit_Framework_TestCase {
     $object->state = 'd';
     $object->owner = 'woot';
     $this->repository->ingestObject($object);
+
     $this->assertTrue($object instanceof FedoraObject);
     $this->assertEquals($id, $object->id);
     $this->assertEquals('foo', $object->label);
     $this->assertEquals('D', $object->state);
     $this->assertEquals('woot', $object->owner);
+
     $this->repository->purgeObject($object->id);
   }
 
@@ -55,10 +62,55 @@ class RepositoryTest extends PHPUnit_Framework_TestCase {
     $object->state = 'd';
     $object->owner = 'woot';
     $this->repository->ingestObject($object);
+
+    $this->assertTrue($object instanceof FedoraObject);
+    $this->assertEquals($id, $object->id);
+    $this->assertEquals('foo', $object->label);
+    $this->assertEquals('D', $object->state);
+    $this->assertEquals('woot', $object->owner);
+
+    $this->repository->purgeObject($object->id);
+  }
+
+  public function testIngestUuid() {
+    $object = $this->repository->constructObject(NULL, TRUE);
+    $object->label = 'foo';
+    $object->state = 'd';
+    $object->owner = 'woot';
+    $this->repository->ingestObject($object);
+
+    $id_array = explode(':', $object->id);
+    $is_uuid = preg_match('/^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-(:?8|9|a|b)[a-f0-9]{3}-[a-f0-9]{12}\z/',
+                          $id_array[1]);
+
+    $this->assertEquals(TRUE, $is_uuid);
     $this->assertTrue($object instanceof FedoraObject);
     $this->assertEquals('foo', $object->label);
     $this->assertEquals('D', $object->state);
     $this->assertEquals('woot', $object->owner);
+
+    $this->repository->purgeObject($object->id);
+  }
+
+  public function testIngestUuidNamespaced() {
+    $namespace = FedoraTestHelpers::randomString(10);
+    $object = $this->repository->constructObject($namespace, TRUE);
+    $object->label = 'foo';
+    $object->state = 'd';
+    $object->owner = 'woot';
+    $this->repository->ingestObject($object);
+
+    $id_array = explode(':', $object->id);
+    $is_uuid = preg_match('/^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-(:?8|9|a|b)[a-f0-9]{3}-[a-f0-9]{12}\z/',
+                          $id_array[1]);
+
+    $this->assertEquals($namespace, $id_array[0]);
+    $this->assertEquals(TRUE, $is_uuid);
+    $this->assertTrue($object instanceof FedoraObject);
+    $this->assertEquals('foo', $object->label);
+    $this->assertEquals('D', $object->state);
+    $this->assertEquals('woot', $object->owner);
+
     $this->repository->purgeObject($object->id);
   }
 
